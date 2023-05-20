2874ddf73fddc369d73113744907b110f0345698
rohit190397 <123259359+rohit190397@users.noreply.github.com>
2023-05-20 11:14:37 +0530

    Update mysqlfeed.py

--- mysqlfeed.py
+++ mysqlfeed.py
@@ -7,9 +7,9 @@ conn =pymysql.connect(database="rohit",user="rohit",password="pass",host="localh
 #Create a MySQL Cursor to that executes the SQLs
 cur=conn.cursor()
 #Create a dictonary containing the fields, name, age and place
-data={'topic':'iot_sensor','sensor value':10.10}
+data={'topic':'iot_sensor','sensorvalue':10.10}
 #Execute the SQL to write data to the database
-cur.execute("INSERT INTO <tablename>(name, age, place)VALUES(%(name)s,%(age)s,%(place)s);",data)
+cur.execute("INSERT INTO rohit(topic, sensorvalue)VALUES(%(topic)s,%(sensorvalue)s);",data)
 #Close the cursor
 cur.close()
 #Commit the data to the database
